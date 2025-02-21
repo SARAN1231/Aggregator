2448a5263eb23f2cec8e70d01c8d3a0a1539ec0a
SARAN1231 <saranprakash3112@gmail.com>
2025-02-21 16:39:24 +0530

    Starting Aggregator

---
+++ src/main/java/com/saran/Agreegator/AgreegatorApplication.java
@@ -0,0 +1,13 @@
+package com.saran.Agreegator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgreegatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgreegatorApplication.class, args);
+	}
+
+}
